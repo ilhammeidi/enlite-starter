3501003b62ce4f850bfef5b37f1801e9e0aa766d
Ilham Meidi Lenovo <ilham.meidi@gmail.com>
2024-07-13 18:36:04 +0700

    restore firebase config

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyBT9MFsTOdDG5Cx0skAOtef8uzIBLJQ2xA',
-  authDomain: 'enlite-3a841.firebaseapp.com',
-  databaseURL: 'https://enlite-3a841.firebaseio.com',
-  projectId: 'enlite-3a841',
-  storageBucket: 'enlite-3a841.appspot.com',
-  messagingSenderId: '37352684805'
+  apiKey: 'ccraSyBT9MFsTOdDG5Cx0skAOtef8uzIBLJQ2xA',
+  authDomain: 'yourapp-1234e.firebaseapp.com',
+  databaseURL: 'https://youreapp-1234e.firebaseio.com',
+  projectId: 'youreapp-1234e',
+  storageBucket: 'youreapp-1234e.appspot.com',
+  messagingSenderId: '37325684805'
 };
 
 export default firebaseConfig;
